be49ce4cd161076f5daf3e085fde76296bc6b8e6
distantorigin <distantorigin@distantorig.in>
2025-10-22 23:34:31 -0500

    Initial updater test.

---
+++ switch-to-dev.bat
@@ -0,0 +1,7 @@
+@echo off
+REM Quick script to switch to dev channel
+echo Switching to DEV update channel...
+echo.
+updater.exe --switch-channel dev
+echo.
+pause
---
+++ switch-to-stable.bat
@@ -0,0 +1,7 @@
+@echo off
+REM Quick script to switch to stable channel
+echo Switching to STABLE update channel...
+echo.
+updater.exe --switch-channel stable
+echo.
+pause

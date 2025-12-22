331bb6792559bb6b4bd6a8f4ee79bf2bfa34d3a2
Derrick LaRose <25400572+ryanb96@users.noreply.github.com>
2025-12-22 00:20:03 -0600

    Possibly fixed all nothing nearby triggers for archaeology

--- lua/miriani/scripts/archaeology.lua
+++ lua/miriani/scripts/archaeology.lua
@@ -108,7 +108,7 @@ ImportXML([=[
   <trigger
    enabled="y"
    group="archaeology"
-   match="^A level \w+ archaeological dig site scanner reports nothing nearby\.$"
+match="^A level \w+ archaeological dig site scanner (indicates|reports) (nothing nearby|that nothing is buried nearby|that nothing is nearby)\.$"
    regexp="y"
    omit_from_output="y"
    send_to="14"

0abf0a7108b8628e2926ca3cae42e1335246793e
PsudoDeSudo <erick@erosso.net>
2026-02-26 10:06:26 -0500

    Added npc voices for a few more NPCS on Acrylon.

--- lua/miriani/scripts/npc_voices.lua
+++ lua/miriani/scripts/npc_voices.lua
@@ -58,5 +58,60 @@ ImportXML([=[
   <send>mplay("npc/harried_woman_prompt4", "npc", 1)</send>
   </trigger>
 
+  <trigger
+   enabled="y"
+   group="npc_voices"
+   match="^A charming young woman says, &quot;Welcome to Acrylon! How may I be of assistance\?&quot;$"
+   regexp="y"
+   send_to="12"
+   sequence="100"
+  >
+  <send>mplay("npc/charming_woman_intro", "npc", 1)</send>
+  </trigger>
+
+  <trigger
+   enabled="y"
+   group="npc_voices"
+   match="^A charming young woman says, &quot;You're quite welcome! Please come back any time\.&quot;$"
+   regexp="y"
+   send_to="12"
+   sequence="100"
+  >
+  <send>mplay("npc/charming_woman_prompt1", "npc", 1)</send>
+  </trigger>
+
+  <trigger
+   enabled="y"
+   group="npc_voices"
+   match="^A salesman says, &quot;I am happy you asked\.&quot;$"
+   regexp="y"
+   send_to="12"
+   sequence="100"
+  >
+  <send>mplay("npc/salesman_prompt1", "npc", 1)</send>
+  </trigger>
+
+  <trigger
+   enabled="y"
+   group="npc_voices"
+   match="^A salesman says, &quot;Perfect! Let me hand you your card\.&quot;$"
+   regexp="y"
+   send_to="12"
+   sequence="100"
+  >
+  <send>mplay("npc/salesman_prompt2", "npc", 1)</send>
+  </trigger>
+
+  <trigger
+   enabled="y"
+   group="npc_voices"
+   match="^A salesman says, &quot;It appears that you are already registered\.&quot;$"
+   regexp="y"
+   send_to="12"
+   sequence="100"
+  >
+  <send>mplay("npc/salesman_prompt3", "npc", 1)</send>
+  </trigger>
+
   </triggers>
 ]=])

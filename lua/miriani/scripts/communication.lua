d916426acddb43d5834f929e03fda0aba5df9276
JasonHarkness <royalredsox@gmail.com>
2026-06-03 20:38:08 -0400

    Attempting to revert corrupted files.

--- lua/miriani/scripts/communication.lua
+++ lua/miriani/scripts/communication.lua
@@ -876,25 +876,6 @@ ImportXML([=[
   </send>
   </trigger>
 
-  
-  <trigger
-   enabled="y"
-   group="comm"
-   match="^.+? (?:yawns suddenly and collapses to the ground, asleep|drifts off to sleep)\.$"
-   regexp="y"
-   send_to="12"
-  >
-  <send>
-   -- Play yawn social (randomly choose male or female)
- --  local yawn_gender = (math.random(2) == 1) and "male" or "female"
-    --  mplay("social/"..yawn_gender.."/yawn", "socials")
-      mplay("misc/Connections/disconnected", "communication")
-    -- Play collapse social (neuter has collapse sounds)
-   -- mplay("social/neuter/collapse", "socials")
-  </send>
-  </trigger>
-  
-  
   <trigger
    enabled="y"
    group="comm"

54720bcdba3a323b4a34f73dae45d082f7905c10
distantorigin <distantorigin@distantorig.in>
2026-07-29 14:42:42 -0500

    Fix: Fix case on smc

--- lua/miriani/scripts/starmap_scan.lua
+++ lua/miriani/scripts/starmap_scan.lua
@@ -1046,7 +1046,7 @@ return 0
   <trigger
    enabled="y"
    group="starmap"
-   match="^\d+ \d+ \d+, Ship \d+, .+\.$"
+   match="^\d+ \d+ \d+, ship \d+, .+\.$"
    regexp="y"
    send_to="14"
    sequence="100"
